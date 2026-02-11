712e2139df28d14a532bcfcd550d8c1de2b3cc64
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-11 06:49:16 -0500

    Time: 141 ms (5.8%), Space: 31.9 MB (5.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #using counter:
+        nums_count = Counter(nums)
+        for k,freq in nums_count.items():
+            if freq > 1:
+                return True
+        return False
+
+        #using 2 ptrs:
+        '''
+        if len(nums) == 1:
+            return False
+        nums.sort()
+        left,right = 0,1
+        while right<len(nums):
+            if nums[left] == nums[right]:
+                return True
+        return False
+        '''
