76fb0c713f299f92f8d5b529b9c7ec49598afee1
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-13 07:07:59 -0500

    Time: 43 ms (95.46%), Space: 27.1 MB (19.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,35 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ''' Bruteforce:
-        if len(nums)==0:
-            return 0
-        if len(nums) == 1:
-            return 1
-        nums.sort()
-        maxCon = 0
-        temp = 0
-        left = 0
-        right = 1
-        #[1,1,2,3,4,5,5] temp = 0
-        while right < len(nums):
-            if nums[right] - nums[left] == 1:
-                temp += 1
-            elif nums[right] - nums[left] > 1:
-                temp = 0
-            left += 1
-            right += 1
-            maxCon = max(maxCon , temp)
-        return maxCon + 1
-        '''
-
         setN = set(nums)
-        maxCon = 0
+        maxlen = 0
         for n in setN:
+            
             if n-1 not in setN:
-                currCon = 1
-                while n + currCon in setN:
-                    currCon += 1
-                maxCon = max(maxCon, currCon)
-        
-        return maxCon
+                length = 1
+                while n+length in setN:
+                    length += 1
+                
+                maxlen = max(length,maxlen)
+
+        return maxlen
+
