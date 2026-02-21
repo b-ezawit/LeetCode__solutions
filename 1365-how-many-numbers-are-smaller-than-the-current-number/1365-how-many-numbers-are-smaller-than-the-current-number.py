4d0e7ecedbed5223b1e6ce7ac47db5571f690d6a
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-21 09:16:54 -0500

    Time: 0 ms (100%), Space: 12.6 MB (7.66%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -5,24 +5,10 @@ class Solution(object):
         :rtype: List[int]
         """
        
-        nums_sort = sorted(nums)
+        sortNums = sorted(nums)
+        numMap = {}
+        for i,num in enumerate(sortNums):
+            if num not in numMap:
+                numMap[num] = i
 
-        for i in range(len(nums)):
-            count = 0
-            indx_in_sort = nums_sort.index(nums[i]) #if nums[i] = 8, nums_in_sort.index(8) is 4 
-            for j in range(indx_in_sort):
-                if nums_sort[j] < nums[i]:
-                    count += 1
-            nums[i] = count
-        
-        return nums
-            
-
-
-        
-
-
-
-
-
-        
+        return [numMap[num] for num in nums]
