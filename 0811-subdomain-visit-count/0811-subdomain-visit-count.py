49456621141efed5d070287e66038fdf7ea18e9a
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-24 15:57:57 -0500

    Time: 7 ms (46.71%), Space: 19.3 MB (75.44%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -2,19 +2,14 @@ class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         hmap = {}
         for s in cpdomains:
-            dots = s.count(".")
+            dots =  s.count(".")
             s = s.split()
             for _ in range(dots+1):
-                if s[1] not in hmap:
-                    hmap[s[1]] = int(s[0])
-                else:
-                    hmap[s[1]] += int(s[0])
-
+                hmap[s[1]] = hmap.get(s[1] , 0) + int(s[0])
+                
                 if "." in s[1]:
                     dot_indx = s[1].find(".")
-                    s[1] = s[1][dot_indx+1:]        
+                    s[1] = s[1][dot_indx+1:]
         return [f"{val} {key}" for key,val in hmap.items()]
-                
 
-        
         
