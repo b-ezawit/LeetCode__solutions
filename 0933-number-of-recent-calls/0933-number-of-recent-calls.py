0df63c7f00e67d3d18b87b04aa537dd1f03b8ae7
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-03-10 13:18:17 -0500

    Time: 82 ms (31.19%), Space: 17.7 MB (6.7%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -4,18 +4,17 @@ class RecentCounter(object):
         self.requests = []
         self.start = 0
 
+
     def ping(self, t):
         """
         :type t: int
         :rtype: int
         """
-        while self.start < len(self.requests) and t-self.requests[self.start] >  3000:
+        while self.start<len(self.requests) and t-self.requests[self.start] > 3000:
             self.start += 1
-            
-
         self.requests.append(t)
         return len(self.requests) - self.start
-        
+            
 
 
 # Your RecentCounter object will be instantiated and called as such:
