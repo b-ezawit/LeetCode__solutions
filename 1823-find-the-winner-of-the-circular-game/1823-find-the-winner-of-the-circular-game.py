5edc70f288bbba7c9e655d74fe7652d48923654c
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-03-17 10:56:59 -0500

    Time: 4 ms (41.14%), Space: 19.2 MB (85.4%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,14 +1,15 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        nums = [n for n in range(1,n+1)]
-        def soln(arr,k,start):
-            if len(arr) == 1:
-                return arr[0]
-            remove_indx = (start+k-1) % len(arr)
-            arr.pop(remove_indx)
+        nums = [val for val in range(1,n+1)]  
 
-            return soln(arr,k,remove_indx)   
-        return soln(nums,k,0)         
+
+        def soln(arr,count,start):
+            if len(arr) == 1:
+                return arr[0]  
+            remove_indx = (start + count -1)%len(arr)
+            arr.pop(remove_indx)
+            return soln(arr,count,remove_indx)
+        return soln(nums,k,0)
 
 
         
