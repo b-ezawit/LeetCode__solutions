ea756973f4962b023115b7c662f5bba154ee9db3
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-05-02 15:06:46 -0500

    Time: 100 ms (69.59%), Space: 56.9 MB (70.75%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,15 +1,45 @@
-class RandomizedSet:
+class RandomizedSet(object):
 
     def __init__(self):
-        
+        self.values = []
+        self.valueIndx = {} 
 
-    def insert(self, val: int) -> bool:
-        
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.valueIndx:
+            self.valueIndx[val] = len(self.values)
+            self.values.append(val)
+            return True
+        return False
 
-    def remove(self, val: int) -> bool:
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.valueIndx:
+            index = self.valueIndx[val]
+            lastItem = self.values[-1]
+            self.values[index], self.values[-1] = self.values[-1] , self.values[index]
+            self.valueIndx[lastItem] = index
+            del self.valueIndx[val]
+            self.values.pop()
+            return True
         
+        return False
+              
 
-    def getRandom(self) -> int:
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.choice(self.values)
+
+   
+
         
 
 
